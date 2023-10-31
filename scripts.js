fbdee056dea7d06de7aac6ed1f9f5959b0afb7cb
Edson Câmara Filho <edsoncamarafilho07@gmail.com>
2023-10-31 19:15:06 -0300

    [CHANGES] Descomentando o script

--- scripts.js
+++ scripts.js
@@ -8,22 +8,22 @@
 
 
 
-// let menorAltura = Infinity;
-// let maiorAltura = -Infinity; 
+let menorAltura = Infinity;
+let maiorAltura = -Infinity; 
 
-// for (let i = 0; i < 15; i++) {
-//     let altura = parseFloat(prompt(`Digite a altura da pessoa ${i + 1}:`));
+for (let i = 0; i < 15; i++) {
+    let altura = parseFloat(prompt(`Digite a altura da pessoa ${i + 1}:`));
    
-//     if (altura < menorAltura) {
-//         menorAltura = altura;
-//     }
-//     if (altura > maiorAltura) {
-//         maiorAltura = altura;
-//     }
-// }
+    if (altura < menorAltura) {
+        menorAltura = altura;
+    }
+    if (altura > maiorAltura) {
+        maiorAltura = altura;
+    }
+}
 
-// console.log(`A menor altura do grupo é: ${menorAltura} metros`);
-// console.log(`A maior altura do grupo é: ${maiorAltura} metros`);
+document.write(`A menor altura do grupo é: ${menorAltura} metros <br><br>`);
+document.write(`A maior altura do grupo é: ${maiorAltura} metros <br><br>`);
 
 
 
@@ -37,19 +37,19 @@
 // b. Lucas seja maior que Pedro.
 
 
-// let pedroAltura = 150
-// let lucasAltura = 110
-// let anos = 0
+let pedroAltura = 150
+let lucasAltura = 110
+let anos = 0
 
-// while(pedroAltura > lucasAltura) {
+while(pedroAltura > lucasAltura) {
 
-//     pedroAltura += 2
-//     lucasAltura += 3
-//     anos += 1
-// } 
+    pedroAltura += 2
+    lucasAltura += 3
+    anos += 1
+} 
 
-// document.write(`${anos-1} <br>`)
-// document.write(`${anos}`)
+document.write(`${anos-1} <br>`)
+document.write(`${anos} <br>`)
 
 
 // 3. Escreva um algoritmo que faça a tabuada de um número e
@@ -62,13 +62,13 @@
 // ...
 
 
-// const numeroTabuada = Number(prompt('Digite um número:'))
-// const iteracoes = Number(prompt('Digite a quantidade de iteraçoes:'))
+const numeroTabuada = Number(prompt('Digite um número:'))
+const iteracoes = Number(prompt('Digite a quantidade de iteraçoes:'))
 
-// for(let i = 1; i <= iteracoes; i++){
-//     console.log(`${i} x ${numeroTabuada} = ${i * numeroTabuada}`)
+for(let i = 1; i <= iteracoes; i++){
+    console.log(`${i} x ${numeroTabuada} = ${i * numeroTabuada}`)
 
-// }
+}
 
 
 
@@ -77,30 +77,30 @@
 // de 5.
 
 
-// let isMultipleOfThree = false;
-// let isMultipleOfFive = false;
+let isMultipleOfThree = false;
+let isMultipleOfFive = false;
 
-// let algoritmoNumero = 250;
+let algoritmoNumero = 250;
 
-// for (let i = 1; i <= algoritmoNumero; i++) {
+for (let i = 1; i <= algoritmoNumero; i++) {
 
-//     if (i % 3 === 0){
-//         isMultipleOfThree = true;
-//     }
-//     if (i % 5 === 0) {
-//         isMultipleOfFive = true;
-//     }
+    if (i % 3 === 0){
+        isMultipleOfThree = true;
+    }
+    if (i % 5 === 0) {
+        isMultipleOfFive = true;
+    }
 
-//     if(isMultipleOfThree) {
-//         document.write(`O número ${i} é múltiplo de 3. <br><br>`);
-//         isMultipleOfThree = false;
-//     }
+    if(isMultipleOfThree) {
+        document.write(`O número ${i} é múltiplo de 3. <br><br>`);
+        isMultipleOfThree = false;
+    }
 
-//     if (isMultipleOfFive) {
-//         document.write(`O número ${i} é múltiplo de 5. <br><br>`);
-//         isMultipleOfFive = false;
-//     }
-// }
+    if (isMultipleOfFive) {
+        document.write(`O número ${i} é múltiplo de 5. <br><br>`);
+        isMultipleOfFive = false;
+    }
+}
 
 
 
@@ -114,12 +114,12 @@
 // Contagem: 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, FIM!
 
 
-    // let numeroInteiroPositivo = parseInt(Number(prompt('Digite um número positivo ')))
+    let numeroInteiroPositivo = parseInt(Number(prompt('Digite um número positivo ')))
 
-    // for (let i = 0; i <= numeroInteiroPositivo; i++) {
-    //     document.write(`${i}`)
-    // } 
-    // document.write('Fim')
+    for (let i = 0; i <= numeroInteiroPositivo; i++) {
+        document.write(`${i}`)
+    } 
+    document.write('Fim')
 
 
 
@@ -147,29 +147,29 @@
 // positivos, a quantidade de valores negativos e o percentual de
 // valores negativos e positivos.
 
-// let totalSomado = 0
-// let numerosPositivos = 0
-// let numerosNegativos = 0
+let totalSomado = 0
+let numerosPositivos = 0
+let numerosNegativos = 0
 
-// for (let i = 0; i <= 10; i++) {
-//     let lerNumero = Number(prompt("Digite um número:"));
-//     totalSomado += lerNumero
+for (let i = 0; i <= 10; i++) {
+    let lerNumero = Number(prompt("Digite um número:"));
+    totalSomado += lerNumero
 
-//     if (lerNumero >= 0) {
-//         numerosPositivos++
-//     } else {
-//         numerosNegativos++
-//     }
-// }
+    if (lerNumero >= 0) {
+        numerosPositivos++
+    } else {
+        numerosNegativos++
+    }
+}
 
-// let mediaNumeros = totalSomado / 10
-// let porcentagemPositivos = (numerosPositivos / 10) * 100
-// let porcentagemNegativos = (numerosNegativos / 10) * 100
+let mediaNumeros = totalSomado / 10
+let porcentagemPositivos = (numerosPositivos / 10) * 100
+let porcentagemNegativos = (numerosNegativos / 10) * 100
 
-// console.log(`Média aritimetica: ${mediaNumeros}`)
-// console.log(`Total de números POSITIVOS: ${numerosPositivos}`)
-// console.log(`Total de números NEGATIVOS: ${numerosNegativos}`)
-// console.log(`Média de números POSITIVOS: ${porcentagemPositivos}`);
-// console.log(`Média de números NEGATIVOS: ${porcentagemNegativos}`);
+console.log(`Média aritimetica: ${mediaNumeros}`)
+console.log(`Total de números POSITIVOS: ${numerosPositivos}`)
+console.log(`Total de números NEGATIVOS: ${numerosNegativos}`)
+console.log(`Média de números POSITIVOS: ${porcentagemPositivos}`);
+console.log(`Média de números NEGATIVOS: ${porcentagemNegativos}`);
 
  
